abf75d0727207ba3d95acd8237f4a0ec801e61a5
mixersoft <michael@snaphappi.com>
2014-07-14 17:38:33 -0400

    add font-awesome to grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,6 +373,11 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/font-awesome/fonts',
+          src: '*',
+          dest: '<%= yeoman.dist %>/fonts/'  
         }]
       },
       styles: {
